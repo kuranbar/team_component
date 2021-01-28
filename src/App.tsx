cc189f68267a4a5b763099643ae78f840ea11bdf
kuranbar <code.workshop@wp.pl>
2021-01-28 20:10:17 +0100

    updating project core component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Section from "./Components/Section/Section";
+import Slider from "./Components/Slider/Slider";
 
-function App() {
+const App: React.FC = () => {
+  const [flag, setFlag] = React.useState(true);
+  const [slideIndex, setSlideIndex] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {flag ? (
+        <Section
+          flag={flag}
+          setFlag={setFlag}
+          slideIndex={slideIndex}
+          setSlideIndex={setSlideIndex}
+        />
+      ) : (
+        <Slider flag={flag} setFlag={setFlag} slideIndex={slideIndex} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
