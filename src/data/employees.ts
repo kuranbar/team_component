fdf91a4705de8da31ae3c1418bd34cb43491661b
kuranbar <code.workshop@wp.pl>
2021-01-28 20:08:04 +0100

    adding component data

---
+++ src/data/employees.ts
@@ -0,0 +1,70 @@
+import member_1 from "../assets/img/member_1.png";
+import member_2 from "../assets/img/member_2.png";
+import member_3 from "../assets/img/member_3.png";
+import member_4 from "../assets/img/member_4.png";
+import member_5 from "../assets/img/member_5.png";
+
+const Employees = [
+  {
+    name: "Basia Sołtysińska",
+    occupation: "Founder",
+    city: "Warsaw",
+    img: member_1,
+  },
+  {
+    name: "Włas Chorowiec",
+    occupation: "Founder",
+    city: "Warsaw",
+    img: member_2,
+  },
+  {
+    name: "Edyta Leśniewska",
+    occupation: "Head of Human Resources",
+    city: "Warsaw",
+    img: member_3,
+  },
+  {
+    name: "Joanna Pawluk",
+    occupation: "Chief of Growth Office (CGO)",
+    city: "London",
+    img: member_4,
+  },
+  {
+    name: "David Saunders",
+    occupation: "Business Development",
+    city: "London",
+    img: member_5,
+  },
+  {
+    name: "Adam Eldridge",
+    occupation: "Business Development Director\u00A0US",
+    city: "Los Angeles",
+    img: member_1,
+  },
+  {
+    name: "Maha Mahda",
+    occupation: "Chief Business Development Officer\u00A0(CBDO)",
+    city: "Warsaw",
+    img: member_2,
+  },
+  {
+    name: "Elżbieta Kamińska",
+    occupation: "International Client Service\u00A0Manager",
+    city: "Warsaw",
+    img: member_3,
+  },
+  {
+    name: "Karolina Makuch",
+    occupation: "Creation Manager",
+    city: "Warsaw",
+    img: member_4,
+  },
+  {
+    name: "Kamil Bolek",
+    occupation: "Head of Communication",
+    city: "Warsaw",
+    img: member_5,
+  },
+];
+
+export default Employees;
